415b6aecf938e84a6cf7f3b99e1e5c2aff08ec78
Mark Le <pvle.2017@gmail.com>
2020-11-30 14:50:50 +0700

    change times to mul

--- BondingCurve_flattened.sol
+++ BondingCurve_flattened.sol
@@ -3,8 +3,240 @@
 
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// SPDX-License-Identifier: MIT
+
+
+/**
+ * @dev Wrappers over Solidity's arithmetic operations with added overflow
+ * checks.
+ *
+ * Arithmetic operations in Solidity wrap on overflow. This can easily result
+ * in bugs, because programmers usually assume that an overflow raises an
+ * error, which is the standard behavior in high level programming languages.
+ * `SafeMath` restores this intuition by reverting the transaction when an
+ * operation overflows.
+ *
+ * Using this library instead of the unchecked operations eliminates an entire
+ * class of bugs, so it's recommended to use it always.
+ */
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     *
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     *
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return mod(a, b, "SafeMath: modulo by zero");
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts with custom message when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b != 0, errorMessage);
+        return a % b;
+    }
+}
+// SPDX-License-Identifier: MIT
+
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
 
 pragma solidity ^0.6.0;
 
@@ -101,8 +333,8 @@ contract BondingCurve {
 		assert(newX > 0);
 
 		uint256 weiRequired = newY.sub(_y);
-		uint256 weiRequiredBeforeNiftex = weiRequired.div(1000).mul(uint256(1000).add(_feePctToSuppliers));
-		uint256 weiRequiredAfterNiftex = weiRequired.div(1000).mul(uint256(1000).add(_feePctToSuppliers).add(_feePctToNiftex));
+		uint256 weiRequiredBeforeNiftex = weiRequired.div(1000).times(uint256(1000).add(_feePctToSuppliers));
+		uint256 weiRequiredAfterNiftex = weiRequired.div(1000).times(uint256(1000).add(_feePctToSuppliers).add(_feePctToNiftex));
 
 		uint256 actualShardsBeforeNiftex = _x.sub(_k.div(_y.add(weiRequiredBeforeNiftex)));
 		uint256 actualShardsAfterNiftex = _x.sub(_k.div(_y.add(weiRequiredAfterNiftex)));
@@ -256,7 +488,7 @@ contract BondingCurve {
 	}
 
 	function withdrawSuppliedShards(uint256 shardAmount) external {
-		uint256 maxShardsToWithdraw = _shardSuppliers._mappingShardLPTokens[msg.sender].div(_shardSuppliers._totalShardLPTokens).mul(_shardSuppliers._totalSuppliedShardsPlusFeesToSuppliers);
+		uint256 maxShardsToWithdraw = _shardSuppliers._mappingShardLPTokens[msg.sender].div(_shardSuppliers._totalShardLPTokens).times(_shardSuppliers._totalSuppliedShardsPlusFeesToSuppliers);
 		require(
 			shardAmount <= maxShardsToWithdraw,
 			"Cannot withdraw more than your current amount of shards in the pool"
@@ -277,7 +509,7 @@ contract BondingCurve {
 		uint256 otherShardLPTokens = _shardSuppliers._totalShardLPTokens.sub(_shardSuppliers._mappingShardLPTokens[msg.sender]);
 		uint256 remainingShardsOfCurrentLP = maxShardsToWithdraw.sub(shardAmount);
 		uint256 remainingShardsOfOtherLPs = _shardSuppliers._totalSuppliedShardsPlusFeesToSuppliers.sub(maxShardsToWithdraw);
-		uint256 newShardLPTokensOfCurrentLP = remainingShardsOfCurrentLP.div(remainingShardsOfOtherLPs).mul(otherShardLPTokens);
+		uint256 newShardLPTokensOfCurrentLP = remainingShardsOfCurrentLP.div(remainingShardsOfOtherLPs).times(otherShardLPTokens);
 
 		_shardSuppliers._totalSuppliedShardsPlusFeesToSuppliers = _shardSuppliers._totalSuppliedShardsPlusFeesToSuppliers.sub(shardAmount);
 		_shardSuppliers._totalShardLPTokens = otherShardLPTokens.add(newShardLPTokensOfCurrentLP);
@@ -303,7 +535,7 @@ contract BondingCurve {
 	}
 
 	function withdrawSuppliedEther(uint256 ethAmount) external {
-		uint256 maxEthToWithdraw = _ethSuppliers._mappingEthLPTokens[msg.sender].div(_ethSuppliers._totalEthLPTokens).mul(_ethSuppliers._totalSuppliedEthPlusFeesToSuppliers);
+		uint256 maxEthToWithdraw = _ethSuppliers._mappingEthLPTokens[msg.sender].div(_ethSuppliers._totalEthLPTokens).times(_ethSuppliers._totalSuppliedEthPlusFeesToSuppliers);
 
 		require(
 			ethAmount <= maxEthToWithdraw,
@@ -323,7 +555,7 @@ contract BondingCurve {
 		uint256 otherEthLPTokens = _ethSuppliers._totalEthLPTokens.sub(_ethSuppliers._mappingEthLPTokens[msg.sender]);
 		uint256 remainingEthOfCurrentLP = maxEthToWithdraw.sub(ethAmount);
 		uint256 remainingEthOfOtherLPs = _ethSuppliers._totalSuppliedEthPlusFeesToSuppliers.sub(maxEthToWithdraw);
-		uint256 newEthLPTokensOfCurrentLP = remainingEthOfCurrentLP.div(remainingEthOfOtherLPs).mul(otherEthLPTokens);
+		uint256 newEthLPTokensOfCurrentLP = remainingEthOfCurrentLP.div(remainingEthOfOtherLPs).times(otherEthLPTokens);
 
 		_ethSuppliers._totalSuppliedEthPlusFeesToSuppliers = _ethSuppliers._totalSuppliedEthPlusFeesToSuppliers.sub(ethAmount);
 		_ethSuppliers._totalEthLPTokens = otherEthLPTokens.add(newEthLPTokensOfCurrentLP);
--- readme.md
+++ readme.md
@@ -1,9 +1,1 @@
-./solidityFlattener.pl --contractsdir=contracts/L2 --remapdir "contracts/L2/@openzeppelin=./node_modules/@openzeppelin" --mainsol=ERC721L2Mapping.sol --verbose --outputsol=./flattenedContracts/L2/ERC721L2Mapping_flattened.sol
-
-./solidityFlattener.pl --contractsdir=contracts --remapdir "contracts/@openzeppelin=./node_modules/@openzeppelin" --mainsol=ERC721L2Mapping.sol --verbose
-
-./solidityFlattener.pl --contractsdir=contracts --remapdir "contracts/@openzeppelin=./node_modules/@openzeppelin" --mainsol=ShardFactory.sol --verbose
-
-./solidityFlattener.pl --contractsdir=contracts/L2 --remapdir "contracts/L2/@openzeppelin=./node_modules/@openzeppelin" --mainsol=L2ShardRegistry.sol --verbose
-
-./solidityFlattener.pl --contractsdir=rescueContracts --remapdir "rescueContracts/@openzeppelin=./node_modules/@openzeppelin" --mainsol=MaticDistributor.sol --verbose --outputsol=./flattenedContracts/rescueContracts/MaticDistributor_flattened.sol
+./solidityFlattener.pl --contractsdir=contracts --remapdir "contracts/@openzeppelin=./node_modules/@openzeppelin" --mainsol=BondingCurve.sol --verbose
