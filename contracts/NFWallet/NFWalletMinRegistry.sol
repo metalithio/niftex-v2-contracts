11a845be513e2edfb0e040bb638292c58bc54497
Hadrien Croubois <hc@iex.ec>
2020-12-10 11:43:02 +0100

    missing initialization

---
+++ contracts/NFWallet/NFWalletMinRegistry.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../utils/CloneFactory.sol";
+import "./NFWalletMin.sol";
+
+contract NFWalletMinRegistry is CloneFactory, ERC721
+{
+    constructor()
+    ERC721("Minimal Non Fungible Wallet", "mNFW")
+    CloneFactory(address(new NFWalletMin()))
+    {}
+
+    function mintWallet(address to) public returns (address instance)
+    {
+        instance = _clone();
+        instance.initialize(address(this));
+        _mint(to, uint256(instance));
+    }
+}
