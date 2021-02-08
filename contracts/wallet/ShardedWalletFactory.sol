50acde02d8331c82ce99b1eefca97fdf395c0f0a
jmahhh <hubertjoel@gmail.com>
2021-02-08 09:53:34 +0800

    remove mapping of fractionalizations

--- contracts/wallet/ShardedWalletFactory.sol
+++ contracts/wallet/ShardedWalletFactory.sol
@@ -2,15 +2,11 @@
 
 pragma solidity ^0.7.0;
 
-import "@openzeppelin/contracts/utils/EnumerableSet.sol";
 import "../utils/CloneFactory.sol";
 import "./ShardedWallet.sol";
 
 contract ShardedWalletFactory is CloneFactory
 {
-    using EnumerableSet for EnumerableSet.AddressSet;
-    EnumerableSet.AddressSet private _wallets;
-
     constructor()
     CloneFactory(address(new ShardedWallet()))
     {}
@@ -26,18 +22,5 @@ contract ShardedWalletFactory is CloneFactory
     {
         instance = _clone();
         ShardedWallet(payable(instance)).initialize(governance_, owner_, name_, symbol_, artistWallet_);
-        _wallets.add(instance);
-    }
-
-    function isWallet(address wallet) external view returns (bool) {
-        return _wallets.contains(wallet);
-    }
-
-    function getWallet(uint256 id) external view returns (address) {
-        return _wallets.at(id);
-    }
-
-    function walletCount() external view returns (uint256) {
-        return _wallets.length();
     }
 }
