594b280092c8f4cdcc8260348e2d1411b68e30bc
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-02-21 15:29:45 +0100

    fix packages & config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,9 @@
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-truffle5");
 require("@nomiclabs/hardhat-solhint");
+require("@openzeppelin/hardhat-upgrades");
 require("solidity-coverage");
-require('@openzeppelin/hardhat-upgrades');
-require('dotenv').config();
-
-const INFURA_URL = process.env.INFURA_URL;
-
-// Replace this private key with your Ropsten account private key
-// To export your private key from Metamask, open Metamask and
-// go to Account Details > Export Private Key
-// Be aware of NEVER putting real Ether into testing accounts
-const NIFTEX_PRIVATE_KEY = process.env.NIFTEX_PRIVATE_KEY;
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.7.5",
@@ -21,10 +13,20 @@ module.exports = {
       runs: 999,
     },
   },
-  networks: {
-    rinkeby: {
-      url: `${INFURA_URL}`,
-      accounts: [`0x${NIFTEX_PRIVATE_KEY}`]
-    }
-  }
+  networks: {},
 };
+
+if ([ 'INFURA_URL', 'NIFTEX_PRIVATE_KEY' ].every(key => key in process.env)) {
+  // Replace this private key with your Ropsten account private key
+  // To export your private key from Metamask, open Metamask and
+  // go to Account Details > Export Private Key
+  // Be aware of NEVER putting real Ether into testing accounts
+  module.exports.networks.rinkeby = {
+    url: process.env.INFURA_URL,
+    accounts: [
+      process.env.NIFTEX_PRIVATE_KEY.startsWith('0x')
+        ? process.env.NIFTEX_PRIVATE_KEY
+        : '0x' + process.env.NIFTEX_PRIVATE_KEY,
+    ],
+  }
+}
