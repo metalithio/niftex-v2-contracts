81572d625c917613a49e70e7968bdabfbc66ecc7
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-03-08 11:40:58 +0100

    update deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,40 +5,38 @@ async function main() {
   console.log(`Deploying contracts with the account: ${deployer.address}`);
   console.log(`Account balance: ${ethers.utils.formatEther(await deployer.getBalance())} ${ethers.constants.EtherSymbol}`);
 
-  // Deploy Factory
-  const Factory = await ethers.getContractFactory("ShardedWalletFactory");
-  const factory = await Factory.deploy();
-  console.log(`Factory address: ${factory.address}`);
-
-  const template = await factory.template();
-
-  // Deploy Governance
-  const Governance = await ethers.getContractFactory("Governance");
-  // const governance = await Governance.deploy();
-  const governance = await upgrades.deployProxy(Governance);
-  await governance.deployed();
-  console.log(`Governance address: ${governance.address}`);
+  // Deploy ShardedWallet
+  const ShardedWallet = await ethers.getContractFactory("ShardedWallet");
+  const shardedwallet = await ShardedWallet.deploy();
+  console.log(`ShardedWallet address: ${shardedwallet.address}`);
 
   // Deploy BondingCurve
   const BondingCurve = await ethers.getContractFactory("BondingCurve3");
   const bondingcurve = await BondingCurve.deploy();
   console.log(`BondingCurve address: ${bondingcurve.address}`);
 
+  // Deploy Governance
+  const Governance = await ethers.getContractFactory("Governance");
+  const governance = await upgrades.deployProxy(Governance);
+  await governance.deployed();
+  console.log(`Governance address: ${governance.address}`);
+
   // Deploy and whitelist modules
   console.log("Deploying modules:");
   const MODULE_ROLE = await governance.MODULE_ROLE();
   const modules = await Object.entries({
-    "action":        "ActionModule",
-    "buyout":        "BuyoutModule",
-    "crowdsale":     "FixedPriceSaleModule",
-    "multicall":     "MulticallModule",
-    "tokenreceiver": "TokenReceiverModule",
+    "action":            "ActionModule",
     "basicdistribution": "BasicDistributionModule",
+    "buyout":            "BuyoutModule",
+    "crowdsale":         "FixedPriceSaleModule",
+    "factory":           "ShardedWalletFactory",
+    "multicall":         "MulticallModule",
+    "tokenreceiver":     "TokenReceiverModule",
   }).reduce(
     async (accAsPromise, [key, name ]) => {
       const acc    = await accAsPromise;
       const Module = await ethers.getContractFactory(name);
-      const module = await Module.deploy(template);
+      const module = await Module.deploy(shardedwallet.address);
       await governance.grantRole(MODULE_ROLE, module.address);
       console.log(` - ${name}: ${module.address}`);
       return Object.assign(acc, { [ key ]: module });
@@ -64,17 +62,17 @@ async function main() {
   // Set config
   console.log("Configuring governance");
   for ([ key, value ] of Object.entries({
-    [ await modules.action.ACTION_AUTH_RATIO()          ]: ethers.utils.parseEther('0.01'),
-    [ await modules.buyout.BUYOUT_AUTH_RATIO()          ]: ethers.utils.parseEther('0.01'),
-    [ await modules.action.ACTION_DURATION()            ]: 50400,
-    [ await modules.buyout.BUYOUT_DURATION()            ]: 50400,
-    [ await modules.crowdsale.CURVE_TEMPLATE()          ]: bondingcurve.address,
-    [ await modules.crowdsale.PCT_SHARDS_NIFTEX()       ]: ethers.utils.parseEther('0.0'),
-    [ await modules.crowdsale.PCT_ETH_TO_CURVE()        ]: ethers.utils.parseEther('0.20'),
-    [ await bondingcurve.PCT_FEE_NIFTEX()               ]: ethers.utils.parseEther('0.001'),
-    [ await bondingcurve.PCT_FEE_ARTIST()               ]: ethers.utils.parseEther('0.001'),
-    [ await bondingcurve.PCT_FEE_SUPPLIERS()            ]: ethers.utils.parseEther('0.003'),
-    [ await bondingcurve.LIQUIDITY_TIMELOCK()           ]: 100800,
+    [ await modules.action.ACTION_AUTH_RATIO()    ]: ethers.utils.parseEther('0.01'),
+    [ await modules.buyout.BUYOUT_AUTH_RATIO()    ]: ethers.utils.parseEther('0.01'),
+    [ await modules.action.ACTION_DURATION()      ]: 50400,
+    [ await modules.buyout.BUYOUT_DURATION()      ]: 50400,
+    [ await modules.crowdsale.CURVE_TEMPLATE()    ]: bondingcurve.address,
+    [ await modules.crowdsale.PCT_SHARDS_NIFTEX() ]: ethers.utils.parseEther('0.0'),
+    [ await modules.crowdsale.PCT_ETH_TO_CURVE()  ]: ethers.utils.parseEther('0.20'),
+    [ await bondingcurve.PCT_FEE_NIFTEX()         ]: ethers.utils.parseEther('0.001'),
+    [ await bondingcurve.PCT_FEE_ARTIST()         ]: ethers.utils.parseEther('0.001'),
+    [ await bondingcurve.PCT_FEE_SUPPLIERS()      ]: ethers.utils.parseEther('0.003'),
+    [ await bondingcurve.LIQUIDITY_TIMELOCK()     ]: 100800,
   }))
   {
     console.log(` - ${key}: ${value}`)
@@ -84,9 +82,9 @@ async function main() {
   console.log("setting global only keys");
 
   for ([ key, value ] of Object.entries({
-    [ 0xedde61aea0459bc05d70dd3441790ccfb6c17980a380201b00eca6f9ef50452a ]: true, // ALLOW_UPGRADE_GOVERNANCE
-    [ await modules.crowdsale.PCT_SHARDS_NIFTEX()  ]: true,
-    [ await bondingcurve.PCT_FEE_NIFTEX() ]: true,
+    [ await shardedwallet.ALLOW_GOVERNANCE_UPGRADE() ]: true,
+    [ await modules.crowdsale.PCT_SHARDS_NIFTEX()    ]: true,
+    [ await bondingcurve.PCT_FEE_NIFTEX()            ]: true,
   }))
   {
     console.log(` - ${key}: ${value}`)
