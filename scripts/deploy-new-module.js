84fc6e4428017cd5b86215fa84b9884fcf6311bf
Mark Le <markle@Marks-MacBook-Pro.local>
2021-04-28 16:15:04 +0700

    right naming for swmanagermodule

--- scripts/deploy-new-module.js
+++ scripts/deploy-new-module.js
@@ -21,7 +21,7 @@ async function main() {
     // "multicall":           "MulticallModule",
     // "tokenreceiver":       "TokenReceiverModule",
     // "erc20managermodule":  "ERC20ManagerModule",
-    "swmanagermodule":     "SwManagerModule",
+    "swmanagermodule":     "SWManagerModule",
   }).reduce(
     async (accAsPromise, [key, name ]) => {
       const acc    = await accAsPromise;
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -41,7 +41,7 @@ async function main() {
     "multicall":           "MulticallModule",
     "tokenreceiver":       "TokenReceiverModule",
     "erc20managermodule":  "ERC20ManagerModule",
-    "swmanagermodule":     "SwManagerModule",
+    "swmanagermodule":     "SWManagerModule",
   }).reduce(
     async (accAsPromise, [key, name ]) => {
       const acc    = await accAsPromise;
