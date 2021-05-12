64b6f0a095986b5c4f975c2da7252cc2b486abf3
Mark Le <markle@Marks-MacBook-Pro.local>
2021-05-12 21:15:05 +0700

    all tests in MasterChef passed

--- test/702_masterchef.js
+++ test/702_masterchef.js
@@ -1,5 +1,6 @@
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
+const { expectRevert } = require('@openzeppelin/test-helpers');
 const { advanceBlockTo } = require("../utils/test-timer");
 
 describe("MasterChef", function () {
@@ -31,26 +32,26 @@ describe("MasterChef", function () {
     const devaddr = await this.chef.devaddr()
     const owner = await this.frac.owner()
 
-    expect(frac).to.equal(this.frac.address)
-    expect(devaddr).to.equal(this.dev.address)
-    expect(owner).to.equal(this.chef.address)
+    assert.equal(frac, this.frac.address)
+    assert.equal(devaddr, this.dev.address)
+    assert.equal(owner, this.chef.address)
   })
 
   it("should allow dev and only dev to update dev", async function () {
     this.chef = await this.MasterChef.deploy(this.frac.address, this.dev.address, "1000", "0", "1000")
     await this.chef.deployed()
 
-    expect(await this.chef.devaddr()).to.equal(this.dev.address)
+    assert.equal(await this.chef.devaddr(), this.dev.address)
 
-    await expect(this.chef.connect(this.bob).dev(this.bob.address, { from: this.bob.address })).to.be.revertedWith("dev: wut?")
+    await expectRevert(this.chef.connect(this.bob).dev(this.bob.address, { from: this.bob.address }),"dev: wut?");
 
     await this.chef.connect(this.dev).dev(this.bob.address, { from: this.dev.address })
 
-    expect(await this.chef.devaddr()).to.equal(this.bob.address)
+    assert.equal(await this.chef.devaddr(), this.bob.address)
 
     await this.chef.connect(this.bob).dev(this.alice.address, { from: this.bob.address })
 
-    expect(await this.chef.devaddr()).to.equal(this.alice.address)
+    assert.equal(await this.chef.devaddr(), this.alice.address)
   })
 
   context("With ERC/LP token added to the field", function () {
@@ -83,11 +84,11 @@ describe("MasterChef", function () {
 
       await this.chef.connect(this.bob).deposit(0, "100")
 
-      expect(await this.lp.balanceOf(this.bob.address)).to.equal("900")
+      assert.equal(await this.lp.balanceOf(this.bob.address), "900");
 
       await this.chef.connect(this.bob).emergencyWithdraw(0)
 
-      expect(await this.lp.balanceOf(this.bob.address)).to.equal("1000")
+      assert.equal(await this.lp.balanceOf(this.bob.address), "1000");
     })
 
     it("should give out FRACs only after farming time", async function () {
@@ -104,26 +105,26 @@ describe("MasterChef", function () {
       await advanceBlockTo("89")
 
       await this.chef.connect(this.bob).deposit(0, "0") // block 90
-      expect(await this.frac.balanceOf(this.bob.address)).to.equal("0")
+      assert.equal(await this.frac.balanceOf(this.bob.address), "0");
       await advanceBlockTo("94")
 
       await this.chef.connect(this.bob).deposit(0, "0") // block 95
-      expect(await this.frac.balanceOf(this.bob.address)).to.equal("0")
+      assert.equal(await this.frac.balanceOf(this.bob.address), "0");
       await advanceBlockTo("99")
 
       await this.chef.connect(this.bob).deposit(0, "0") // block 100
-      expect(await this.frac.balanceOf(this.bob.address)).to.equal("0")
+      assert.equal(await this.frac.balanceOf(this.bob.address), "0");
       await advanceBlockTo("100")
 
       await this.chef.connect(this.bob).deposit(0, "0") // block 101
-      expect(await this.frac.balanceOf(this.bob.address)).to.equal("1000")
+      assert.equal(await this.frac.balanceOf(this.bob.address), "1000");
 
       await advanceBlockTo("104")
       await this.chef.connect(this.bob).deposit(0, "0") // block 105
 
-      expect(await this.frac.balanceOf(this.bob.address)).to.equal("5000")
-      expect(await this.frac.balanceOf(this.dev.address)).to.equal("500")
-      expect(await this.frac.totalSupply()).to.equal("5500")
+      assert.equal(await this.frac.balanceOf(this.bob.address), "5000");
+      assert.equal(await this.frac.balanceOf(this.dev.address), "500");
+      assert.equal(await this.frac.totalSupply(), "5500");
     })
 
     it("should not distribute FRACs if no one deposit", async function () {
@@ -134,21 +135,21 @@ describe("MasterChef", function () {
       await this.chef.add("100", this.lp.address, true)
       await this.lp.connect(this.bob).approve(this.chef.address, "1000")
       await advanceBlockTo("199")
-      expect(await this.frac.totalSupply()).to.equal("0")
+      assert.equal(await this.frac.totalSupply(), "0");
       await advanceBlockTo("204")
-      expect(await this.frac.totalSupply()).to.equal("0")
+      assert.equal(await this.frac.totalSupply(), "0");
       await advanceBlockTo("209")
       await this.chef.connect(this.bob).deposit(0, "10") // block 210
-      expect(await this.frac.totalSupply()).to.equal("0")
-      expect(await this.frac.balanceOf(this.bob.address)).to.equal("0")
-      expect(await this.frac.balanceOf(this.dev.address)).to.equal("0")
-      expect(await this.lp.balanceOf(this.bob.address)).to.equal("990")
+      assert.equal(await this.frac.totalSupply(), "0")
+      assert.equal(await this.frac.balanceOf(this.bob.address), "0")
+      assert.equal(await this.frac.balanceOf(this.dev.address), "0")
+      assert.equal(await this.lp.balanceOf(this.bob.address), "990")
       await advanceBlockTo("219")
       await this.chef.connect(this.bob).withdraw(0, "10") // block 220
-      expect(await this.frac.totalSupply()).to.equal("11000")
-      expect(await this.frac.balanceOf(this.bob.address)).to.equal("10000")
-      expect(await this.frac.balanceOf(this.dev.address)).to.equal("1000")
-      expect(await this.lp.balanceOf(this.bob.address)).to.equal("1000")
+      assert.equal(await this.frac.totalSupply(), "11000")
+      assert.equal(await this.frac.balanceOf(this.bob.address), "10000")
+      assert.equal(await this.frac.balanceOf(this.dev.address), "1000")
+      assert.equal(await this.lp.balanceOf(this.bob.address), "1000")
     })
 
     it("should distribute FRACs properly for each staker", async function () {
@@ -180,22 +181,22 @@ describe("MasterChef", function () {
       //   MasterChef should have the remaining: 10000 - 5666 = 4334
       await advanceBlockTo("319")
       await this.chef.connect(this.alice).deposit(0, "10", { from: this.alice.address })
-      expect(await this.frac.totalSupply()).to.equal("11000")
-      expect(await this.frac.balanceOf(this.alice.address)).to.equal("5666")
-      expect(await this.frac.balanceOf(this.bob.address)).to.equal("0")
-      expect(await this.frac.balanceOf(this.carol.address)).to.equal("0")
-      expect(await this.frac.balanceOf(this.chef.address)).to.equal("4334")
-      expect(await this.frac.balanceOf(this.dev.address)).to.equal("1000")
+      assert.equal(await this.frac.totalSupply(), "11000")
+      assert.equal(await this.frac.balanceOf(this.alice.address), "5666")
+      assert.equal(await this.frac.balanceOf(this.bob.address), "0")
+      assert.equal(await this.frac.balanceOf(this.carol.address), "0")
+      assert.equal(await this.frac.balanceOf(this.chef.address), "4334")
+      assert.equal(await this.frac.balanceOf(this.dev.address), "1000")
       // Bob withdraws 5 LPs at block 330. At this point:
       //   Bob should have: 4*2/3*1000 + 2*2/6*1000 + 10*2/7*1000 = 6190
       await advanceBlockTo("329")
       await this.chef.connect(this.bob).withdraw(0, "5", { from: this.bob.address })
-      expect(await this.frac.totalSupply()).to.equal("22000")
-      expect(await this.frac.balanceOf(this.alice.address)).to.equal("5666")
-      expect(await this.frac.balanceOf(this.bob.address)).to.equal("6190")
-      expect(await this.frac.balanceOf(this.carol.address)).to.equal("0")
-      expect(await this.frac.balanceOf(this.chef.address)).to.equal("8144")
-      expect(await this.frac.balanceOf(this.dev.address)).to.equal("2000")
+      assert.equal(await this.frac.totalSupply(), "22000")
+      assert.equal(await this.frac.balanceOf(this.alice.address), "5666")
+      assert.equal(await this.frac.balanceOf(this.bob.address), "6190")
+      assert.equal(await this.frac.balanceOf(this.carol.address), "0")
+      assert.equal(await this.frac.balanceOf(this.chef.address), "8144")
+      assert.equal(await this.frac.balanceOf(this.dev.address), "2000")
       // Alice withdraws 20 LPs at block 340.
       // Bob withdraws 15 LPs at block 350.
       // Carol withdraws 30 LPs at block 360.
@@ -205,18 +206,18 @@ describe("MasterChef", function () {
       await this.chef.connect(this.bob).withdraw(0, "15", { from: this.bob.address })
       await advanceBlockTo("359")
       await this.chef.connect(this.carol).withdraw(0, "30", { from: this.carol.address })
-      expect(await this.frac.totalSupply()).to.equal("55000")
-      expect(await this.frac.balanceOf(this.dev.address)).to.equal("5000")
+      assert.equal(await this.frac.totalSupply(), "55000")
+      assert.equal(await this.frac.balanceOf(this.dev.address), "5000")
       // Alice should have: 5666 + 10*2/7*1000 + 10*2/6.5*1000 = 11600
-      expect(await this.frac.balanceOf(this.alice.address)).to.equal("11600")
+      assert.equal(await this.frac.balanceOf(this.alice.address), "11600")
       // Bob should have: 6190 + 10*1.5/6.5 * 1000 + 10*1.5/4.5*1000 = 11831
-      expect(await this.frac.balanceOf(this.bob.address)).to.equal("11831")
+      assert.equal(await this.frac.balanceOf(this.bob.address), "11831")
       // Carol should have: 2*3/6*1000 + 10*3/7*1000 + 10*3/6.5*1000 + 10*3/4.5*1000 + 10*1000 = 26568
-      expect(await this.frac.balanceOf(this.carol.address)).to.equal("26568")
+      assert.equal(await this.frac.balanceOf(this.carol.address), "26568")
       // All of them should have 1000 LPs back.
-      expect(await this.lp.balanceOf(this.alice.address)).to.equal("1000")
-      expect(await this.lp.balanceOf(this.bob.address)).to.equal("1000")
-      expect(await this.lp.balanceOf(this.carol.address)).to.equal("1000")
+      assert.equal(await this.lp.balanceOf(this.alice.address), "1000")
+      assert.equal(await this.lp.balanceOf(this.bob.address), "1000")
+      assert.equal(await this.lp.balanceOf(this.carol.address), "1000")
     })
 
     it("should give proper FRACs allocation to each pool", async function () {
@@ -234,16 +235,16 @@ describe("MasterChef", function () {
       await advanceBlockTo("419")
       await this.chef.add("20", this.lp2.address, true)
       // Alice should have 10*1000 pending reward
-      expect(await this.chef.pendingFrac(0, this.alice.address)).to.equal("10000")
+      assert.equal(await this.chef.pendingFrac(0, this.alice.address), "10000")
       // Bob deposits 10 LP2s at block 425
       await advanceBlockTo("424")
       await this.chef.connect(this.bob).deposit(1, "5", { from: this.bob.address })
       // Alice should have 10000 + 5*1/3*1000 = 11666 pending reward
-      expect(await this.chef.pendingFrac(0, this.alice.address)).to.equal("11666")
+      assert.equal(await this.chef.pendingFrac(0, this.alice.address), "11666")
       await advanceBlockTo("430")
       // At block 430. Bob should get 5*2/3*1000 = 3333. Alice should get ~1666 more.
-      expect(await this.chef.pendingFrac(0, this.alice.address)).to.equal("13333")
-      expect(await this.chef.pendingFrac(1, this.bob.address)).to.equal("3333")
+      assert.equal(await this.chef.pendingFrac(0, this.alice.address), "13333")
+      assert.equal(await this.chef.pendingFrac(1, this.bob.address), "3333")
     })
 
     it("should stop giving bonus FRACs after the bonus period ends", async function () {
@@ -257,11 +258,11 @@ describe("MasterChef", function () {
       await this.chef.connect(this.alice).deposit(0, "10", { from: this.alice.address })
       // At block 605, she should have 1000*10 + 100*5 = 10500 pending.
       await advanceBlockTo("605")
-      expect(await this.chef.pendingFrac(0, this.alice.address)).to.equal("10500")
+      assert.equal(await this.chef.pendingFrac(0, this.alice.address), "10500")
       // At block 606, Alice withdraws all pending rewards and should get 10600.
       await this.chef.connect(this.alice).deposit(0, "0", { from: this.alice.address })
-      expect(await this.chef.pendingFrac(0, this.alice.address)).to.equal("0")
-      expect(await this.frac.balanceOf(this.alice.address)).to.equal("10600")
+      assert.equal(await this.chef.pendingFrac(0, this.alice.address), "0")
+      assert.equal(await this.frac.balanceOf(this.alice.address), "10600")
     })
   })
 })
