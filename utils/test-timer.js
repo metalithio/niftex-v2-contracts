2b4dbee3bcdac625b85b1e58a299f60337b0a35c
Mark Le <markle@Marks-MacBook-Pro.local>
2021-05-12 20:44:38 +0700

    masterchef 0.8.0 before replacing safemath

---
+++ utils/test-timer.js
@@ -0,0 +1,62 @@
+const { ethers } = require("hardhat")
+
+const { BigNumber } = ethers
+
+function advanceBlock() {
+  return ethers.provider.send("evm_mine", [])
+}
+
+function advanceBlockTo(blockNumber) {
+  for (let i = await ethers.provider.getBlockNumber(); i < blockNumber; i++) {
+    await advanceBlock()
+  }
+}
+
+function increase(value) {
+  await ethers.provider.send("evm_increaseTime", [value.toNumber()])
+  await advanceBlock()
+}
+
+function latest() {
+  const block = await ethers.provider.getBlock("latest")
+  return BigNumber.from(block.timestamp)
+}
+
+function advanceTimeAndBlock(time) {
+  await advanceTime(time)
+  await advanceBlock()
+}
+
+function advanceTime(time) {
+  await ethers.provider.send("evm_increaseTime", [time])
+}
+
+const duration = {
+  seconds: function (val) {
+    return BigNumber.from(val)
+  },
+  minutes: function (val) {
+    return BigNumber.from(val).mul(this.seconds("60"))
+  },
+  hours: function (val) {
+    return BigNumber.from(val).mul(this.minutes("60"))
+  },
+  days: function (val) {
+    return BigNumber.from(val).mul(this.hours("24"))
+  },
+  weeks: function (val) {
+    return BigNumber.from(val).mul(this.days("7"))
+  },
+  years: function (val) {
+    return BigNumber.from(val).mul(this.days("365"))
+  },
+}
+
+module.exports = {
+  advanceBlockTo,
+  increase,
+  latest,
+  advanceTimeAndBlock,
+  advanceTime,
+  duration
+};
