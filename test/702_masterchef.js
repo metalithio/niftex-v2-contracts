94703755b084d8f4a36c8b96b1af66bda4c41def
Mark Le <markle@Marks-MacBook-Pro.local>
2021-05-25 16:35:07 +0700

    update masterchef test cases, probably need to toWei

--- test/702_masterchef.js
+++ test/702_masterchef.js
@@ -190,73 +190,92 @@ contract("MasterChef", function (accounts) {
       assert.equal(await this.lp.balanceOf(this.bob.address), "1000")
     })
 
-    // it("should distribute FRACs properly for each staker", async function () {
-    //   // 100 per block farming rate starting at block 300 with bonus until block 1000
-    //   this.chef = await this.MasterChef.deploy(this.frac.address, this.dev.address, "100", "300", "1000")
-    //   await this.chef.deployed()
-    //   await this.frac.transferOwnership(this.chef.address)
-    //   await this.chef.add("100", this.lp.address, true)
-    //   await this.lp.connect(this.alice).approve(this.chef.address, "1000", {
-    //     from: this.alice.address,
-    //   })
-    //   await this.lp.connect(this.bob).approve(this.chef.address, "1000", {
-    //     from: this.bob.address,
-    //   })
-    //   await this.lp.connect(this.carol).approve(this.chef.address, "1000", {
-    //     from: this.carol.address,
-    //   })
-    //   // Alice deposits 10 LPs at block 310
-    //   await advanceBlockTo("309")
-    //   await this.chef.connect(this.alice).deposit(0, "10", { from: this.alice.address })
-    //   // Bob deposits 20 LPs at block 314
-    //   await advanceBlockTo("313")
-    //   await this.chef.connect(this.bob).deposit(0, "20", { from: this.bob.address })
-    //   // Carol deposits 30 LPs at block 318
-    //   await advanceBlockTo("317")
-    //   await this.chef.connect(this.carol).deposit(0, "30", { from: this.carol.address })
-    //   // Alice deposits 10 more LPs at block 320. At this point:
-    //   //   Alice should have: 4*1000 + 4*1/3*1000 + 2*1/6*1000 = 5666
-    //   //   MasterChef should have the remaining: 10000 - 5666 = 4334
-    //   await advanceBlockTo("319")
-    //   await this.chef.connect(this.alice).deposit(0, "10", { from: this.alice.address })
-    //   assert.equal(await this.frac.totalSupply(), "11000")
-    //   assert.equal(await this.frac.balanceOf(this.alice.address), "5666")
-    //   assert.equal(await this.frac.balanceOf(this.bob.address), "0")
-    //   assert.equal(await this.frac.balanceOf(this.carol.address), "0")
-    //   assert.equal(await this.frac.balanceOf(this.chef.address), "4334")
-    //   assert.equal(await this.frac.balanceOf(this.dev.address), "1000")
-    //   // Bob withdraws 5 LPs at block 330. At this point:
-    //   //   Bob should have: 4*2/3*1000 + 2*2/6*1000 + 10*2/7*1000 = 6190
-    //   await advanceBlockTo("329")
-    //   await this.chef.connect(this.bob).withdraw(0, "5", { from: this.bob.address })
-    //   assert.equal(await this.frac.totalSupply(), "22000")
-    //   assert.equal(await this.frac.balanceOf(this.alice.address), "5666")
-    //   assert.equal(await this.frac.balanceOf(this.bob.address), "6190")
-    //   assert.equal(await this.frac.balanceOf(this.carol.address), "0")
-    //   assert.equal(await this.frac.balanceOf(this.chef.address), "8144")
-    //   assert.equal(await this.frac.balanceOf(this.dev.address), "2000")
-    //   // Alice withdraws 20 LPs at block 340.
-    //   // Bob withdraws 15 LPs at block 350.
-    //   // Carol withdraws 30 LPs at block 360.
-    //   await advanceBlockTo("339")
-    //   await this.chef.connect(this.alice).withdraw(0, "20", { from: this.alice.address })
-    //   await advanceBlockTo("349")
-    //   await this.chef.connect(this.bob).withdraw(0, "15", { from: this.bob.address })
-    //   await advanceBlockTo("359")
-    //   await this.chef.connect(this.carol).withdraw(0, "30", { from: this.carol.address })
-    //   assert.equal(await this.frac.totalSupply(), "55000")
-    //   assert.equal(await this.frac.balanceOf(this.dev.address), "5000")
-    //   // Alice should have: 5666 + 10*2/7*1000 + 10*2/6.5*1000 = 11600
-    //   assert.equal(await this.frac.balanceOf(this.alice.address), "11600")
-    //   // Bob should have: 6190 + 10*1.5/6.5 * 1000 + 10*1.5/4.5*1000 = 11831
-    //   assert.equal(await this.frac.balanceOf(this.bob.address), "11831")
-    //   // Carol should have: 2*3/6*1000 + 10*3/7*1000 + 10*3/6.5*1000 + 10*3/4.5*1000 + 10*1000 = 26568
-    //   assert.equal(await this.frac.balanceOf(this.carol.address), "26568")
-    //   // All of them should have 1000 LPs back.
-    //   assert.equal(await this.lp.balanceOf(this.alice.address), "1000")
-    //   assert.equal(await this.lp.balanceOf(this.bob.address), "1000")
-    //   assert.equal(await this.lp.balanceOf(this.carol.address), "1000")
-    // })
+    it("should distribute FRACs properly for each staker", async function () {
+      // 1 per block farming rate starting at block 300 with bonus until block 10300
+      this.chef = await this.MasterChef.new(
+        this.frac.address, 
+        this.dev.address, 
+        "1", 
+        "300", 
+        "10300",
+        "20300",
+        web3.utils.toWei('2')
+      );
+
+      await this.frac.transfer(this.chef.address, '30000', { from: this.minter.address });
+
+      await this.chef.add("100", this.lp.address, true)
+      await this.lp.approve(this.chef.address, "1000", {
+        from: this.alice.address,
+      })
+      await this.lp.approve(this.chef.address, "1000", {
+        from: this.bob.address,
+      })
+      await this.lp.approve(this.chef.address, "1000", {
+        from: this.carol.address,
+      })
+      // Alice deposits 10 LPs at block 310
+      await advanceBlockTo("309")
+      await this.chef.deposit(0, "10", { from: this.alice.address })
+      // Bob deposits 20 LPs at block 314
+      await advanceBlockTo("313")
+      await this.chef.deposit(0, "20", { from: this.bob.address })
+      console.log('accFracPerShare @314: ', new BigNumber((await this.chef.poolInfo(0)).accFracPerShare).toFixed());
+      // Carol deposits 30 LPs at block 318
+      await advanceBlockTo("317")
+      await this.chef.deposit(0, "30", { from: this.carol.address })
+      console.log('accFracPerShare @318: ', new BigNumber((await this.chef.poolInfo(0)).accFracPerShare).toFixed());
+      await advanceBlockTo("319")
+      // console.log('pending Frac for alice: ', new BigNumber(await this.chef.pendingFrac(0, this.alice.address)).toFixed());
+      await this.chef.deposit(0, "10", { from: this.alice.address }) // block 320
+      console.log('accFracPerShare @320: ', new BigNumber((await this.chef.poolInfo(0)).accFracPerShare).toFixed());
+      // accFracPerShare for pool 0 @320: [2 * 4 * 1e12 / 10] + [2* 4 * 1e12 / 30] + [2 * 2 * 1e12 / 60]
+      // = 800000000000 + 266666666666 + 66666666666 = 1133333333332
+      assert.equal((await this.chef.poolInfo(0)).accFracPerShare, '1133333333332');
+      assert.equal(await this.frac.balanceOf(this.alice.address), '11'); // = 1133333333332 * 10 / 1e12 = 11
+      assert.equal(await this.frac.balanceOf(this.chef.address), "29989"); // 30k - 11 = 29989
+      // Bob withdraws 5 LPs at block 330. At this point:
+      await advanceBlockTo("329")
+      await this.chef.withdraw(0, "5", { from: this.bob.address });
+      console.log('accFracPerShare @330: ', new BigNumber((await this.chef.poolInfo(0)).accFracPerShare).toFixed());
+      // accFracPerShare for pool 0 @330: [2 * 4 * 1e12 / 10] + [2* 4 * 1e12 / 30] + [2 * 2 * 1e12 / 60] + [2 * 10 * 1e12 / 70]
+      // = 800000000000 + 266666666666 + 66666666666 + 285714285714 = 1419047619046
+      assert.equal((await this.chef.poolInfo(0)).accFracPerShare, '1419047619046');
+      assert.equal(await this.frac.balanceOf(this.bob.address), '12'); // 1419047619046 * 20 / 1e12 - 800000000000 * 20 / 1e12 = 28 - 16 = 12
+      assert.equal(await this.frac.balanceOf(this.chef.address), "29977"); // 29989 - 12
+      // Alice withdraws 20 LPs at block 340.
+      // Bob withdraws 15 LPs at block 350.
+      // Carol withdraws 30 LPs at block 360.
+      await advanceBlockTo("339")
+      await this.chef.withdraw(0, "20", { from: this.alice.address });
+      console.log('accFracPerShare @340: ', new BigNumber((await this.chef.poolInfo(0)).accFracPerShare).toFixed());
+      // accFracPerShare for pool 0 @340: [2 * 4 * 1e12 / 10] + [2* 4 * 1e12 / 30] + [2 * 2 * 1e12 / 60] + [2 * 10 * 1e12 / 70] + [2 * 10 * 1e12 / 65]
+      // = 800000000000 + 266666666666 + 66666666666 + 285714285714 + 307692307692 = 1726739926738
+      assert.equal((await this.chef.poolInfo(0)).accFracPerShare, '1726739926738');
+      // assert.equal(await this.frac.balanceOf(this.bob.address), '12'); // 1419047619046 * 20 / 1e12 - 800000000000 * 20 / 1e12 = 28 - 16 = 12
+      // assert.equal(await this.frac.balanceOf(this.chef.address), "29977"); // 29989 - 12
+
+
+      // await advanceBlockTo("349")
+      // await this.chef.withdraw(0, "15", { from: this.bob.address })
+
+
+
+      // await advanceBlockTo("359")
+      // await this.chef.withdraw(0, "30", { from: this.carol.address })
+      // assert.equal(await this.frac.totalSupply(), "55000")
+      // assert.equal(await this.frac.balanceOf(this.dev.address), "5000")
+      // // Alice should have: 5666 + 10*2/7*1000 + 10*2/6.5*1000 = 11600
+      // assert.equal(await this.frac.balanceOf(this.alice.address), "11600")
+      // // Bob should have: 6190 + 10*1.5/6.5 * 1000 + 10*1.5/4.5*1000 = 11831
+      // assert.equal(await this.frac.balanceOf(this.bob.address), "11831")
+      // // Carol should have: 2*3/6*1000 + 10*3/7*1000 + 10*3/6.5*1000 + 10*3/4.5*1000 + 10*1000 = 26568
+      // assert.equal(await this.frac.balanceOf(this.carol.address), "26568")
+      // // All of them should have 1000 LPs back.
+      // assert.equal(await this.lp.balanceOf(this.alice.address), "1000")
+      // assert.equal(await this.lp.balanceOf(this.bob.address), "1000")
+      // assert.equal(await this.lp.balanceOf(this.carol.address), "1000")
+    })
 
     // it("should give proper FRACs allocation to each pool", async function () {
     //   // 100 per block farming rate starting at block 400 with bonus until block 1000
