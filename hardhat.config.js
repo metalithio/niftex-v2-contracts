a381b2e4a6dc9a77e72bd2004bf563631b5920a1
Mark Le <markle@Marks-MacBook-Pro.local>
2021-07-06 15:33:45 +0700

    updated to 50 gwei...

--- hardhat.config.js
+++ hardhat.config.js
@@ -35,7 +35,7 @@ module.exports.networks.mainnet = {
       ? process.env.MAINNET_PRIVATE_KEY
       : '0x' + process.env.MAINNET_PRIVATE_KEY,
   ],
-  gasPrice: 14000000000, // 14 gwei
+  gasPrice: 50000000000, // 50 gwei
   timeout: 60*1000*10,
 }
 
