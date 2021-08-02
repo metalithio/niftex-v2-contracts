ec56d0e87d21318037093e1ea74b83b4b81b7f25
Mark Le <markle@Marks-MacBook-Pro.local>
2021-08-02 08:24:28 +0700

    skeleton for ETH - NFT NFT Bid Module test

--- test/910_nft_bid_module_eth.js
+++ test/910_nft_bid_module_eth.js
@@ -1,7 +1,7 @@
 const { BN, constants, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
 
 contract('Workflow', function (accounts) {
-	const [ admin, user1, user2, user3, other1, other2, other3 ] = accounts;
+	const [ admin, user1, user2, user3, bidder1, bidder2, bidder3 ] = accounts;
 
 	const ShardedWallet        = artifacts.require('ShardedWallet');
 	const Governance           = artifacts.require('Governance');
@@ -12,6 +12,7 @@ contract('Workflow', function (accounts) {
 		Factory:       { artifact: artifacts.require('ShardedWalletFactory')    },
 		Multicall:     { artifact: artifacts.require('MulticallModule')         },
 		TokenReceiver: { artifact: artifacts.require('TokenReceiverModule')     },
+		NftBid:        { artifact: artifacts.require('NftBidModule')            },
 	};
 	const Mocks = {
 		ERC20:     { artifact: artifacts.require('ERC20Mock'),   args: [ 'ERC20Mock', '20']                                      },
@@ -45,6 +46,8 @@ contract('Workflow', function (accounts) {
 		await this.governance.setGlobalConfig(await this.modules.buyout.BUYOUT_AUTH_RATIO(), web3.utils.toWei('0.01'));
 		await this.governance.setGlobalConfig(await this.modules.action.ACTION_DURATION(), 50400);
 		await this.governance.setGlobalConfig(await this.modules.buyout.BUYOUT_DURATION(), 50400);
+		await this.governance.setGlobalConfig(await this.modules.nftbid.NFT_TRANSFER_FEE_NIFTEX(), web3.utils.toWei('0.01')); // 1% niftex fee
+		await this.governance.setGlobalConfig(await this.modules.nftbid.NFT_TRANSFER_FEE_ARTIST(), web3.utils.toWei('0.025')); // 2.5% artist fee
 		for (funcSig of Object.keys(this.modules.tokenreceiver.methods).map(web3.eth.abi.encodeFunctionSignature))
 		{
 			await this.governance.setGlobalModule(funcSig, this.modules.tokenreceiver.address);
@@ -58,7 +61,89 @@ contract('Workflow', function (accounts) {
 		console.log('factory deployment:', gasUsedFactory);
 	});
 
-	describe('Workflow ETH - ERC721', function () {
+	describe('Initialize', function () {
+		it('perform', async function () {
+			const { receipt } = await this.modules.factory.mintWallet(
+				this.governance.address,      // governance_
+				user1,                        // owner_
+				'Tokenized NFT',              // name_
+				'TNFT',                       // symbol_
+				constants.ZERO_ADDRESS        // artistWallet_
+			);
+			instance = await ShardedWallet.at(receipt.logs.find(({ event}) => event == 'NewInstance').args.instance);
+			console.log('tx.receipt.gasUsed:', receipt.gasUsed);
+		});
+
+		after(async function () {
+			assert.equal(await instance.owner(),                            user1);
+			assert.equal(await instance.name(),                             'Tokenized NFT');
+			assert.equal(await instance.symbol(),                           'TNFT');
+			assert.equal(await instance.decimals(),                         '18');
+			assert.equal(await instance.totalSupply(),                      '0');
+			assert.equal(await instance.balanceOf(instance.address),        '0');
+			assert.equal(await instance.balanceOf(user1),                   '0');
+			assert.equal(await instance.balanceOf(user2),                   '0');
+			assert.equal(await instance.balanceOf(user3),                   '0');
+			assert.equal(await instance.balanceOf(other1),                  '0');
+			assert.equal(await instance.balanceOf(other2),                  '0');
+			assert.equal(await instance.balanceOf(other3),                  '0');
+			assert.equal(await web3.eth.getBalance(instance.address),       web3.utils.toWei('0'));
+		});
+	});
+
+	describe('Prepare tokens', function () {
+		it('perform', async function () {
+			await this.mocks.erc721.mint(instance.address, 1);
+			await this.mocks.erc1155.mint(instance.address, 1, 1, '0x');
+		});
+
+		after(async function () {
+			assert.equal(await instance.owner(),                            user1);
+			assert.equal(await instance.name(),                             'Tokenized NFT');
+			assert.equal(await instance.symbol(),                           'TNFT');
+			assert.equal(await instance.decimals(),                         '18');
+			assert.equal(await instance.totalSupply(),                      '0');
+			assert.equal(await instance.balanceOf(instance.address),        '0');
+			assert.equal(await instance.balanceOf(user1),                   '0');
+			assert.equal(await instance.balanceOf(user2),                   '0');
+			assert.equal(await instance.balanceOf(user3),                   '0');
+			assert.equal(await instance.balanceOf(other1),                  '0');
+			assert.equal(await instance.balanceOf(other2),                  '0');
+			assert.equal(await instance.balanceOf(other3),                  '0');
+			assert.equal(await this.mocks.erc721.ownerOf(1),                instance.address);
+			assert.equal(await web3.eth.getBalance(instance.address),       web3.utils.toWei('0'));
+		});
+	});
+
+	describe('Setup basic distribution module', function () {
+		it('perform', async function () {
+			const { receipt } = await this.modules.crowdsale.setup(
+				instance.address,
+				[[ user1, 8 ], [ user2, 2 ]],
+				{ from: user1 }
+			);
+			console.log('tx.receipt.gasUsed:', receipt.gasUsed);
+		});
+
+		after(async function () {
+			assert.equal(await instance.owner(),                            constants.ZERO_ADDRESS);
+			assert.equal(await instance.name(),                             'Tokenized NFT');
+			assert.equal(await instance.symbol(),                           'TNFT');
+			assert.equal(await instance.decimals(),                         '18');
+			assert.equal(await instance.totalSupply(),                      '10');
+			assert.equal(await instance.balanceOf(instance.address),        '0');
+			assert.equal(await instance.balanceOf(user1),                   '8');
+			assert.equal(await instance.balanceOf(user2),                   '2');
+			assert.equal(await instance.balanceOf(user3),                   '0');
+			assert.equal(await instance.balanceOf(other1),                  '0');
+			assert.equal(await instance.balanceOf(other2),                  '0');
+			assert.equal(await instance.balanceOf(other3),                  '0');
+			assert.equal(await this.mocks.erc721.ownerOf(1),                instance.address);
+			assert.equal(await web3.eth.getBalance(instance.address),       web3.utils.toWei('0'));
+		});
+	});
+
+	describe('Workflow NftBidModule ETH - ERC721', function () {
 		before(async function() {
 			await this.mocks.erc721.mint(user1, 1);
 		});
@@ -160,7 +245,7 @@ contract('Workflow', function (accounts) {
 		});
 	});
 
-	describe('Workflow ETH - ERC1155', function () {
+	describe('Workflow NftBidModule ETH - ERC1155', function () {
 		before(async function() {
 			await this.mocks.erc1155.mint(user1, 1, 100, '0x');
 		});
